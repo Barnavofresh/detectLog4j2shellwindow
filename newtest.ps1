749f5fbea7b042195d6da516729a56c00e413a89
Barnavofresh <97096993+Barnavofresh@users.noreply.github.com>
2022-01-04 12:02:20 +0000

    Add files via upload

---
+++ newtest.ps1
@@ -0,0 +1,5 @@
+﻿Get-WmiObject Win32_process -filter 'name = "powershell.exe"' | foreach-object { $_.SetPriority(64) }
+
+if($env:PROCESSOR_ARCHITECTURE -eq "x86"){gci 'C:\' -rec -force -include *.jar -ea 0 | foreach {select-string "JndiLookup.class" $_} | select -exp Path}Else{gci 'C:\' -rec -force -include *.jar -ea 0 | foreach {select-string "JndiLookup.class" $_} | select -exp Path}
+
+if($env:PROCESSOR_ARCHITECTURE -eq "x86"){gci 'D:\' -rec -force -include *.jar -ea 0 | foreach {select-string "JndiLookup.class" $_} | select -exp Path}Else{gci 'D:\' -rec -force -include *.jar -ea 0 | foreach {select-string "JndiLookup.class" $_} | select -exp Path}
